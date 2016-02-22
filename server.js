d0c445acfe817936524e13ca50b23660fdd4903b
Micah Wells <micahp0506@gmail.com>
2016-02-22 09:46:46 -0600

    Added body-parser and additional routes

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 'use strict';
 
-const express = require('express');
 
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'jade');
 
+//Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/', (req, res) => {
+app.get('/login', (req, res) => {
   res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    res.redirect('/');
 });
 
 app.listen(PORT, () => {
